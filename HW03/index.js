92df3ab7784811761e0e15321b0132a3e75c7a44
JunkPop <gameman7654321@gmail.com>
2024-06-24 01:48:54 +0300

    hw 03 added

---
+++ HW03/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const dataPath = path.join(__dirname, "counters.json");
+const app = express();
+const port = 3000;
+
+function loadCounters() {
+  try {
+    const data = fs.readFileSync(dataPath);
+    return JSON.parse(data);
+  } catch (error) {
+    // If the file does not exist return standard values
+    return { home: 0, about: 0 };
+  }
+}
+
+function saveCounters(counters) {
+  fs.writeFileSync(dataPath, JSON.stringify(counters, null, 2));
+}
+
+let counters = loadCounters(); // starting values
+
+app.get("/", (req, res) => {
+  counters.home += 1;
+  saveCounters(counters);
+  res.send(`<h1>Home Page</h1>
+    <p>Main count: ${counters.home}</p>`);
+});
+
+app.get("/about", (req, res) => {
+  counters.about += 1;
+  saveCounters(counters);
+  res.send(`<h1>About Page</h1>
+    <p>About count: ${counters.about}</p>`);
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on port: ${port}`);
+});
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 create a simple http server, <br>
 there are 2 pages
 with counters on each of them.
+
+## HW02 - publish a package to npm
+
+create a module for npm js
+
+> npm i profile_generator_for_gb
